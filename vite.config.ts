dc5b80f99fdd2bcb48244c9d6ca36c2b3304ed4d
Pramod Ramamurthy <pramodramamurthy@Pramods-MacBook-Pro-2.local>
2025-07-14 22:40:04 +0530

    Latest updates

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Novetek/", 
+  base: "/novetek_final/", 
   server: {
     host: "::",
     port: 8080,
@@ -20,3 +20,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
